ccb54f93cb6616f0f68d9d44c1918475dc1a343c
John Nyokabi <jwnyokabi@gmail.com>
2022-09-09 00:37:50 -0700

    add message to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,9 @@
  *
  * Return: Always 0 (Success)
  */  
+int write(void)
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n);
 	return (1);
 }
