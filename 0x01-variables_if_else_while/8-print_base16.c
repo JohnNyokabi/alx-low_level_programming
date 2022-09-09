0591eac0bc82844d6b26736f39d814f9b32b9d81
John Nyokabi <jwnyokabi@gmail.com>
2022-09-09 09:55:48 -0700

    base 16 hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return; Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	char low;
+
+	for (x = '0'; x <= '9'; x++)
+		putchar(x);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+	return (0);
+}
