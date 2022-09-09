93df4c3637e9af76397170ccb2aa49d9c7ebe0b0
John Nyokabi <jwnyokabi@gmail.com>
2022-09-09 05:51:10 +0000

    assidn size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,12 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("char size: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("int size: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("long int size: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("long long int size: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("float size: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
 	return (0);
 
 }
