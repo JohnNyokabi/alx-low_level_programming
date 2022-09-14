9518c72dfba65e8693003dfcd837982ae9b152a7
John Nyokabi <jwnyokabi@gmail.com>
2022-09-14 17:30:28 +0000

    first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,22 +10,38 @@
 int main(void)
 {
 	int i;
-	unsigned long int q0 = 0;
-	unsigned long int q1 = 1, s;
+	unsigned long int q0 = 0, q1 = 1, s;
+	unsigned long int q0_half0, q0_half1, q1_half0, q1_half1;
+	unsigned long int half0, half1;
 
 	for (i = 1; i < 98; i++)
 	{
 		s = q0 + q1;
-		if (i != 98)
-		{
-			printf("%lu, ", s);
-		}
-		else
-		{
-			printf("%lu\n", s);
-		}
+		printf("%lu, ", s);
 		q0 = q1;
 		q1 = s;
 	}
+	q0_half0 = q0 / 10000000000;
+	q1_half0 = q1 / 10000000000;
+	q0_half1 = q0 % 10000000000;
+	q1_half1 = q1 % 10000000000;
+	for (i = 93; i < 99; i++)
+	{
+		half0 = q0_half0 + q1_half0;
+		half1 = q0_half1 + q1_half1;
+		if (q0_half1 + q1_half1 > 9999999999)
+		{
+			half0 += 1;
+			half1 %= 10000000000;
+		}
+		printf("%lu%lu", half0, half1);
+		if (i != 98)
+			printf(", ");
+		q0_half0 = q1_half0;
+		q0_half1 = q1_half1;
+		q1_half0 = half0;
+		q1_half1 = half1;
+	}
+	printf("\n");
 	return (0);
 }
