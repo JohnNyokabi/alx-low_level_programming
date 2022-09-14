437213e7e3ecea45a9c48408e3e8bb81fee33962
John Nyokabi <jwnyokabi@gmail.com>
2022-09-14 04:37:37 +0000

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "_putchar.h"
+#include "main.h"
 
 /**
  * main - Entry point
@@ -17,6 +17,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n')
+	_putchar('\n');
 	return (0);
 }
