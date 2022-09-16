0f385a057514015af2bfd71a5328760ab625ebec
John Nyokabi <jwnyokabi@gmail.com>
2022-09-16 11:03:08 +0000

    replace number with message

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - use numbers 1 - 100 to solve fizz buzz
+ *
+ * Description: Print Fizz for multiples of 3,
+ * Buzz for multiples of 5 and
+ * FizzBuzz for multiples of both.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i < 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
