1dbfa12aabc45d93ff9fd91f62460544e9b684dd
John Nyokabi <jwnyokabi@gmail.com>
2022-09-16 08:16:52 +0000

    print integers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_numbers - function that prints numbers
- * @i: input value
  *
  * Return: 0-9 followed by a new line
  */
@@ -11,6 +10,6 @@ void print_numbers(void)
 	int i;
 
 	for (i = 0, i <= 9; i++)
-		_putchar(i);
+		_putchar(i + '0');
 	_putchar('\n');
 }
