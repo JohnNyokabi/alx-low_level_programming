0a428a7bbfb51090e61f558ff751e22e57a8c5fd
John Nyokabi <jwnyokabi@gmail.com>
2022-09-19 20:08:35 +0000

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - function for printing a string
+ * @str: String to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	if (*str == '\0')
+		return;
+	_putchar("%c", *str);
+	_putchar('\n');
+	_puts(++str);
+}
