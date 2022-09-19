e0971abd8e02a1e3bf6339d20949f143f1f81dec
John Nyokabi <jwnyokabi@gmail.com>
2022-09-19 19:30:23 +0000

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - function that finds string length
+ * @s: String
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s[i] != '\0')
+		i++;
+	return (i);
+}
