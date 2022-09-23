1d9bc23d4ec7a93920df51a51f92e6285bffc943
John Nyokabi <jwnyokabi@gmail.com>
2022-09-23 11:37:19 +0000

    print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,35 +10,33 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i, j, k;
+	int i, j;
 
-	if (size == 0)
-		printf("\n");
-	else
+	for (i = 0; i < size; i += 10)
 	{
-		for (i = 0; i < size; i += 10)
+		printf("%08x: ", i);
+		for (j = 0; j < 10; j++)
 		{
-			printf("%.8x: ", i);
-			for (j = i; j < i + 10; j++)
-			{
-				if (j % 2 == 0)
-					printf(" ");
-				if (j < size)
-					printf("%.2x", *(b + j));
-				else
-					printf(" ");
-			}
-			printf(" ");
-			for (k = i; k < i + 10; k++)
-			{
-				if (k >= size)
-					break;
-				if (*(b + k) < 32 || *(b + k) > 126)
-					printf("%c", '.');
-				else
-					printf("%c", *(b + k));
-			}
-			printf("\n");
+			if ((j + i) >= size)
+				printf(" ");
+			else
+				printf("%02x", *(b + j + i));
+			if  ((j % 2) != 0 && j != 0)
+				printf(" ");
 		}
+		for (j = 0; j < 10; j++)
+		{
+			if ((j + i) >= size)
+				break;
+			else if (*(b + j + i) >= 31 && *(b + j + i) <= 126)
+				printf("%c", *(b + j + i));
+			else
+				printf(".");
+		}
+		if (i >= size)
+			continue;
+		printf("\n");
 	}
+	if (size <= 0)
+		printf("\n");
 }
