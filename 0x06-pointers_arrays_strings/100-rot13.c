41cb67f04a01062ebe771aeec27e4759a8372e08
John Nyokabi <jwnyokabi@gmail.com>
2022-09-23 00:04:25 +0000

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,14 +9,28 @@
  */
 char *rot13(char *s)
 {
-	int i, j;
+	int i = 0, j;
 
-	char *c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *d[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *c[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L', 'M',
+		'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+		'U', 'V', 'W', 'X', 'Y', 'Z', 'a',
+		'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm', 'n', 'o',
+		'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z'};
+	char *d[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G',
+		'H', 'I', 'J', 'K', 'L', 'M', 'n',
+		'o', 'p', 'q', 'r', 's', 't', 'u',
+		'v', 'w', 'x', 'y', 'z', 'a', 'b',
+		'c', 'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm'};
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i])
 	{
-		for (j = 0; c[j] != '\0'; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == c[j])
 			{
@@ -24,6 +38,7 @@ char *rot13(char *s)
 				break;
 			}
 		}
+		i++;
 	}
 	return (s);
 }
